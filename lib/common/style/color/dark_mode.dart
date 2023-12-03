03475c306192a4782330ac2d335bf0a1d63ba487
안강민 <hkkm612@gmail.com>
2023-12-03 15:34:17 +0900

    create. [App] 기본 폴더 구조 생성

---
+++ lib/common/style/color/dark_mode.dart
@@ -0,0 +1,5 @@
+import 'package:flutter/material.dart';
+
+class DarkMode {
+  static Color PRIMARY_COLOR = Colors.deepPurple;
+}
---
+++ lib/common/style/colors.dart
@@ -0,0 +1,2 @@
+export 'package:moodswing_client/common/style/color/light_mode.dart';
+export 'package:moodswing_client/common/style/color/dark_mode.dart';
